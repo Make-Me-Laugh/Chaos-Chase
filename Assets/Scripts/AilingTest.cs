48c1cedb1d224bb7b3a93b96730bb21002c2d73c
ailing35 <hong-ailing@hotmail.com>
2024-01-28 14:40:32 +0800

    Fix chat bubble and pause mechanism

--- Assets/Scripts/AilingTest.cs
+++ Assets/Scripts/AilingTest.cs
@@ -15,13 +15,26 @@ public class AilingTest : MonoBehaviour
     public void QuitGame()
     {
         SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1f;
     }
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            pausePanel.SetActive(true);
+            PauseGame();
         }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
